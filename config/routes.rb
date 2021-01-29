43ad68f140b02aca6dd789bdb849d1c38173e1a7
mghaby <mark.ghaby71@hotmail.com>
2021-01-29 12:14:01 +1100

    routes made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    post '/users', to: 'users#create'
+    get '/users/:id', to: 'users#show'
+    put '/users/:id', to: 'users#update'
+    delete '/users/:id', to: 'users#delete'
+  end
 end
