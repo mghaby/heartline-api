25f00ebfb905744d7a586dba0927706e1b15315c
mghaby <mark.ghaby71@hotmail.com>
2021-02-11 14:10:53 +1100

    fixed password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true
-  validates :password, presence: true
   validates :age, presence: true
   validates :weight, presence: true
   validates :goal_weight, presence: true
