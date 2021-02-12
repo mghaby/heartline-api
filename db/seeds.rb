687ba401439531bbda8d7a02aa6d87c7aa515000
mghaby <mark.ghaby71@hotmail.com>
2021-02-12 15:16:57 +1100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "SLucijan", password: "pass", password_confirmation: "pass", height: 170, weight: 60, age: 18, public: true, water: 2, activity_level: 1.2 , mf: 5, calories: 0, goal_weight: 70, water_count: 0)
+User.create(username: "ARustam", password: "pass", password_confirmation: "pass", height: 180, weight: 70, age: 19, public: true, water: 2, activity_level: 1.375, mf: 5, calories: 0, goal_weight: 75, water_count: 0)
+User.create(username: "IAlberico", password: "pass", password_confirmation: "pass", height: 190, weight: 80, age: 20, public: true, water: 2, activity_level: 1.465, mf: 5, calories: 0, goal_weight: 75, water_count: 0)
+User.create(username: "AMauri", password: "pass", password_confirmation: "pass", height: 200, weight: 90, age: 21, public: true, water: 2, activity_level: 1.55, mf: 5, calories: 0, goal_weight: 76, water_count: 0)
+User.create(username: "HDawn", password: "pass", password_confirmation: "pass", height: 195, weight: 100, age: 22, public: true, water: 2, activity_level: 1.725, mf: 5, calories: 0, goal_weight: 77, water_count: 0)
+User.create(username: "RIsi", password: "pass", password_confirmation: "pass", height: 185, weight: 100, age: 23, public: true, water: 2, activity_level: 1.9, mf: 5, calories: 0, goal_weight: 78, water_count: 0)
+User.create(username: "IYana", password: "pass", password_confirmation: "pass", height: 175, weight: 120, age: 24, public: true, water: 2, activity_level: 1.95, mf: 5, calories: 0, goal_weight: 79, water_count: 0)
+User.create(username: "BDieter", password: "pass", password_confirmation: "pass", height: 165, weight: 130, age: 25, public: true, water: 2, activity_level: 1.2, mf: 5, calories: 0, goal_weight: 80, water_count: 0)
+User.create(username: "SGarfield", password: "pass", password_confirmation: "pass", height: 160, weight: 65, age: 26, public: true, water: 2, activity_level: 1.375, mf: 5, calories: 0, goal_weight: 82, water_count: 0)
+User.create(username: "MSvein", password: "pass", password_confirmation: "pass", height: 155, weight: 55, age: 27, public: true, water: 2, activity_level: 1.465, mf: 5, calories: 0, goal_weight: 70, water_count: 0)
+User.create(username: "EArkhip", password: "pass", password_confirmation: "pass", height: 156, weight: 45, age: 28, public: false, water: 2, activity_level: 1.55, mf: 5, calories: 0, goal_weight: 62, water_count: 0)
+User.create(username: "RAtli", password: "pass", password_confirmation: "pass", height: 166, weight: 40, age: 29, public: false, water: 2, activity_level: 1.725, mf: 5, calories: 0, goal_weight: 60, water_count: 0)
+User.create(username: "BTeodora", password: "pass", password_confirmation: "pass", height: 176, weight: 75, age: 30, public: false, water: 2, activity_level: 1.9, mf: 5, calories: 0, goal_weight: 70, water_count: 0)
+User.create(username: "ZLjupcho", password: "pass", password_confirmation: "pass", height: 186, weight: 85, age: 25, public: false, water: 2, activity_level: 1.95, mf: 5, calories: 0, goal_weight: 70, water_count: 0)
+User.create(username: "MMurphy", password: "pass", password_confirmation: "pass", height: 196, weight: 95, age: 40, public: false, water: 2, activity_level: 1.2, mf: 5, calories: 0, goal_weight: 80, water_count: 0)
+User.create(username: "LMadis", password: "pass", password_confirmation: "pass", height: 206, weight: 105, age: 35, public: false, water: 2, activity_level: 1.375, mf: 5, calories: 0, goal_weight: 85, water_count: 0)
+User.create(username: "RRahab", password: "pass", password_confirmation: "pass", height: 168, weight: 115, age: 42, public: false, water: 2, activity_level: 1.465, mf: 5, calories: 0, goal_weight: 90, water_count: 0)
+User.create(username: "SGiles", password: "pass", password_confirmation: "pass", height: 178, weight: 125, age: 32, public: false, water: 2, activity_level: 1.55, mf: 5, calories: 0, goal_weight: 92, water_count: 0)
+User.create(username: "TTimeo", password: "pass", password_confirmation: "pass", height: 198, weight: 67, age: 31, public: false, water: 2, activity_level: 1.725, mf: 5, calories: 0, goal_weight: 75, water_count: 0)
+User.create(username: "BCennetig", password: "pass", password_confirmation: "pass", height: 158, weight: 69, age: 37, public: false, water: 2, activity_level: 1.9, mf: 5, calories: 0, goal_weight: 60, water_count: 0)
+
